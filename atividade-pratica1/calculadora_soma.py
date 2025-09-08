bd81e3ae806f6ce4d3135fbd85d9019652f1255b
L-jsA <luanajesus1935@hotmail.com>
2025-09-08 13:57:06 -0300

    Concluido 3 novos exercícios

---
+++ atividade-pratica1/calculadora_soma.py
@@ -0,0 +1,11 @@
+# Este programa soma dois números inseridos pelo usuário
+
+#Solicita ao usuário que insira dois números
+numuro1 = 12
+numero2 = 14
+
+#Calcula a soma dos números
+soma = numuro1 + numero2
+
+# Exibe o resultado
+print(soma)
